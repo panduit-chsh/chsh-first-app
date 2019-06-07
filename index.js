8abefb60a34092e9ed46676225fb58c62d81169b
panduit-chsh <chsh@panduit.com>
2019-06-07 15:44:25 -0500

    Changes made to the javascript

--- index.js
+++ index.js
@@ -12,18 +12,13 @@ let logger;
  * @async 
  */
 async function handleEvent(context, type) {
-  const { action } = context.payload;
-  
-  if (action !== 'opened' && action !== 'edited' && action !== 'created') {
-    return;
-  }
   
   if ( type == 'issue' ) {
     const issueComment = context.issue({ body: 'Thanks for opening this issue!' });
     return context.github.issues.createComment(issueComment);
   }
 
-  if ( type =='pull_request.opened' ) {
+  if ( type =='pull_request' ) {
     const prComment = context.pull_request({ body: 'Thanks for opening this pull request!' })
     return context.github.pull_request.createComment(prComment);
   }
@@ -36,6 +31,8 @@ module.exports = robot => {
   app.on('issues', context => handleEvent(context, 'issue'));
   // see https://developer.github.com/v3/activity/events/types/#pullrequestevent
   app.on('pull_request', context => handleEvent(context, 'pull_request'));
+  
+  
   // For more information on building apps:
   // https://probot.github.io/docs/
 
